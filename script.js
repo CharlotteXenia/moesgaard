91005d33928214ac5821d2cc724de6cad6051501
SafaDaoudi <eaasdao@students.eaaa.dk>
2023-12-07 20:31:21 +0100

    Lidt kode af "forsiden"
    Safa

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// VARIABLER (NAVNGIVNING) AF ID'ER FRA HTML
+let start = document.getElementById("trykHer");
+let venstre = document.getElementById("left");
+let højer = document.getElementById("right");
 
+// VARIABLER (NAVNGIVNING) AF ID'ER FRA HTML - HJÆLP FRA CHATGPT
+
+start.addEventListener("click", function(){
+
+  // Den tid det tager for venstre div om at flytte sig
+  venstre.style.animation = "slideIn 2s ease-in-out forwards";
+  
+  // Den tid det tager for højre div om at flytte sig
+  højer.style.animation = "slideOut 2s ease-in-out forwards";
+
+
+  // TrykHer knappen forsvinder når dørene åbner
+  start.style.display = "none";
+});
