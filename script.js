2d1339dfd4ba04ffb56b59aa37ff5fac9a68968d
Ikasy <jlksn@live.dk>
2023-12-11 10:49:27 +0100

    forsøg at implementere splash

--- script.js
+++ script.js
@@ -3,18 +3,60 @@ let start = document.getElementById("trykHer");
 let venstre = document.getElementById("left");
 let højer = document.getElementById("right");
 let nilen = document.getElementById("nilen");
+let altIndhold = document.getElementById("streger");
 
-start.addEventListener("click", function(){
+//bruger querySelector frem for getElementsByTagName for at undgå array. Det er ikke nødvendigt med et array når der kun er en header og en nav
+let header = document.querySelector("header");
+let main = document.querySelector("main");
 
-  // Den tid det tager for venstre div om at flytte sig
-  venstre.style.animation = "slideIn 2s ease-in-out forwards";
+
+// Tæller for at holde styr på nuværende side
+let sideNu = 0;
+
+
+if (sideNu == 0) {
+  altIndhold.style.display ="none";
+  header.style.display = "none";
+  main.style.display = "none";
+
+  start.addEventListener("click", function(){
+
+    // Den tid det tager for venstre div om at flytte sig
+    venstre.style.animation = "slideIn 2s ease-in-out forwards";
+    
+    // Den tid det tager for højre div om at flytte sig
+    højer.style.animation = "slideOut 2s ease-in-out forwards";
   
-  // Den tid det tager for højre div om at flytte sig
-  højer.style.animation = "slideOut 2s ease-in-out forwards";
+  
+    // TrykHer knappen forsvinder + Nilen-billedet kommer frem.
+    start.style.display = "none";
+    nilen.style.display = "block";
+  
+  });
+}
 
 
-  // TrykHer knappen forsvinder + Nilen-billedet kommer frem.
-  start.style.display = "none";
-  nilen.style.display = "flex";
+if (sideNu == 1) {
 
-});
+}
+
+
+if (sideNu == 2) {
+
+}
+
+
+if (sideNu == 3) {
+
+}
+
+
+if (sideNu == 4) {
+
+}
+
+
+if (sideNu == 5) {
+
+}
+
