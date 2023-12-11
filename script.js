b1c47232b90a0e0a29cafbe80b7804dbf0172d2b
Ikasy <jlksn@live.dk>
2023-12-11 11:13:10 +0100

    splash screen færdig

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let venstre = document.getElementById("left");
 let højer = document.getElementById("right");
 let nilen = document.getElementById("nilen");
 let altIndhold = document.getElementById("streger");
+let startIndhold = document.getElementById("start");
 
 //bruger querySelector frem for getElementsByTagName for at undgå array. Det er ikke nødvendigt med et array når der kun er en header og en nav
 let header = document.querySelector("header");
@@ -13,7 +14,7 @@ let main = document.querySelector("main");
 // Tæller for at holde styr på nuværende side
 let sideNu = 0;
 
-
+// Splash screen 
 if (sideNu == 0) {
   altIndhold.style.display ="none";
   header.style.display = "none";
@@ -22,7 +23,7 @@ if (sideNu == 0) {
   start.addEventListener("click", function(){
 
     // Den tid det tager for venstre div om at flytte sig
-    venstre.style.animation = "slideIn 2s ease-in-out forwards";
+    venstre.style.animation = "slideOut 2s ease-in-out forwards";
     
     // Den tid det tager for højre div om at flytte sig
     højer.style.animation = "slideOut 2s ease-in-out forwards";
@@ -31,12 +32,15 @@ if (sideNu == 0) {
     // TrykHer knappen forsvinder + Nilen-billedet kommer frem.
     start.style.display = "none";
     nilen.style.display = "block";
-  
+    sideNu = 1;
   });
 }
 
 
 if (sideNu == 1) {
+  startIndhold.addEventListener("click", function(){
+    
+  });
 
 }
 
