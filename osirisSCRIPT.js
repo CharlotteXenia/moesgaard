709adbd943209e7dc2a34a7f1a3c332ad6190ec7
SafaDaoudi <eaasdao@students.eaaa.dk>
2023-12-12 11:37:27 +0100

    Osiris næsten færdig
    Safa

--- osirisSCRIPT.js
+++ osirisSCRIPT.js
@@ -22,13 +22,13 @@ let Slide5 = document.getElementById("Slide5");
 
 //ARRAY med teksterne
 let tekster = [Slide1,Slide2,Slide3,Slide4,Slide5]
+tekster[0].style.display = "block";
 
 
 //Eventlistener'en + funktionen når man trykker på én af de runde knapper
 prik1.addEventListener("click",function(){
 
 //FOR-loop: Visning af arrayet, hvor rækkefølgen ikke har nogle indfyldelse på det viste.
-
 //Block & NONE: Den nuværende tekst vises (BLOCK), mens den forrige skjules (NONE).
 
 
@@ -73,3 +73,28 @@ prik5.addEventListener("click",function(){
     }
   tekster[4].style.display = "block";
 })
+
+
+
+
+// Splash screen 
+// if (sideNu == 0) {
+//   altIndhold.style.display ="none";
+//   header.style.display = "none";
+//   main.style.display = "none";
+
+//   start.addEventListener("click", function(){
+
+//     // Den tid det tager for venstre div om at flytte sig
+//     venstre.style.animation = "slideOut 2s ease-in-out forwards";
+    
+//     // Den tid det tager for højre div om at flytte sig
+//     højer.style.animation = "slideOut 2s ease-in-out forwards";
+  
+  
+//     // TrykHer knappen forsvinder + Nilen-billedet kommer frem.
+//     start.style.display = "none";
+//     nilen.style.display = "block";
+//     sideNu = 1;
+//   });
+// }
